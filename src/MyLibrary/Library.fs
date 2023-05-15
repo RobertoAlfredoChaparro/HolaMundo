2e7b0281da4f94f6e27fc254b61d148f3af62f37
r a c <alogos@gmail.com>
2023-05-15 17:26:22 -0300

    Subo el ejercicio de la clase donde implementamos el framework de .NET.

---
+++ src/MyLibrary/Library.fs
@@ -0,0 +1,8 @@
+﻿namespace MyLibrary
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+    let imprimeFloat (x:float) =
+        printfn $"resultado: %f{x}"
---
+++ src/MyProject/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+//printfn "Hello from F#"
+
+open MyLibrary  // o importo el modulo con open MyLibrary.Say
+open MyLibrary
+
+[<EntryPoint>]
+// Entry point
+let main args =  
+//    printfn $"Hola Mundo"
+    Say.hello "Rober" 
+
+    Math.imprimeAdd 3 2
+
+    0
